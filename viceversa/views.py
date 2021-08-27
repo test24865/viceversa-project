efd84d75521fc82af6ae33ccfe3ec407ed6dd02d
Denis Panin <denydies@ukr.net>
2021-08-27 23:45:46 +0300

    Add revers str

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def revers(request):
+    a = request.GET['user_text']
+    reversed_text = a[::-1]
+    print(type(a))
+    num_words = len(a.split())
+    return render(request, 'revers.html', {'user_text': a, 'reversed_text': reversed_text, 'num_words': num_words})
